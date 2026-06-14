e3127094f1e27ad4a59529c3539fd958a6db3421
thirdratecyberpunk <blackbul@aston.ac.uk>
2026-06-14 11:37:56 +0100

    Updated difficulty calculation to use 0.5 * supporting difficulty

--- utils/difficulty.js
+++ utils/difficulty.js
@@ -3,10 +3,10 @@
  * given two adversary difficulties
  * @param {*} a -> adversary A difficulty
  * @param {*} b -> adversary B difficulty
- * @param {*} lowWeight -> how much to alter lower bound by (defaults to 0.75, but JE rules are vague)
+ * @param {*} lowWeight -> how much to alter lower bound by (defaults to 0.5, but JE rules are vague)
  * @returns float
  */
-function combineDifficulty(a, b, lowWeight = 0.75) {
+function combineDifficulty(a, b, lowWeight = 0.5) {
   const high = Math.max(a, b);
   const low = Math.min(a, b);
   return high + lowWeight * low;
